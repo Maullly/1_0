ebe382e12c1d83026127a457814b187ac3909ad0
Karol Malinowski <91538196+Maullly@users.noreply.github.com>
2025-10-07 08:09:19 +0000

    1_1.2

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function show_numbers() {
 };
 
 ex1_button.addEventListener('click', show_numbers);
+
+//1_2
+var ex2_text = document.getElementById('ex2_text');
+var ex2_content = document.getElementById('ex2_content');
+
+ex2_text.addEventListener('input', function() {
+  const value = ex2_text.value;
+  if (value.length !== 9) {
+    ex2_content.innerHTML = 'Długość numeru musi być równa 9';
+    ex2_content.style.color = 'red';
+    return;
+  }
+  if (/[a-zA-Z]/.test(value)) {
+    ex2_content.innerHTML = 'Numer nie może zawierać liter';
+    ex2_content.style.color = 'red';
+    return;
+  }
+  if (/[^0-9]/.test(value)) {
+    ex2_content.innerHTML = 'Numer nie może zawierać znaków specjalnych';
+    ex2_content.style.color = 'red';
+    return;
+  }
+  ex2_content.innerHTML = 'Numer telefonu jest poprawny';
+  ex2_content.style.color = 'green';
+});
