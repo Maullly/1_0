e7f6f89a8cb1b5cf61300151c15a2bc7fbddec62
Karol Malinowski <91538196+Maullly@users.noreply.github.com>
2025-10-07 08:06:34 +0000

    1_1.1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //1_1
-var ex1_button=document.getElementById('ex1_button');  
-var ex1_content=document.getElementById('ex1_content');  
+var ex1_button = document.getElementById('ex1_button');
+var ex1_content = document.getElementById('ex1_content');
 
 function show_numbers() {
   const numbers = [];
@@ -9,3 +9,5 @@ function show_numbers() {
   }
   ex1_content.innerHTML = numbers;
 };
+
+ex1_button.addEventListener('click', show_numbers);
